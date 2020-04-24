3ed76e259dc362e9da7af4a027f71d18f202e02c
Daniel190908 <danesz002@gmail.com>
2020-04-24 16:01:57 +0200

    insert first part

---
+++ scripts.js
@@ -0,0 +1,12 @@
+ let name = 'Dani';
+
+ console.log(name + "'s apples");
+ console.log('Hello World!');
+
+ //This is a JavaScript comment
+
+  /*
+  This is a multiline
+  JavaScrpit comment.
+  */
+ 
